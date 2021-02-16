e4217d2669708f696afc63a27eb2877883d99fcc
lewis <lewis@LAPTOP-O3044F35>
2021-02-15 21:49:53 -0700

    fixed some bugs

--- shsim/shsim.c
+++ shsim/shsim.c
@@ -8,14 +8,14 @@
 //main function
 int main( int argc, char *argv[], char *env[ ]){
 	logReset();							//clear log file
-	log("\nmain function:")	
+	logDebug("\nmain function:");	
 	logArgEnv(argc, argv, env);			//log arguments and environment from main functions
 	int argcSim;						//empty integer to simulate argc
-	char *argvSim[];					//empty pointer to array of strings to simulate argv
+	char **argvSim;						//empty pointer to array of strings to simulate argv
 	int i = 1;							
 	while(i){							//main program loop
 		getInput(argcSim, argvSim);			//get input from user
-		log("\nargcSim, argvSim from input:") 
+		logDebug("\nargcSim, argvSim from input:"); 
 		logArgEnv(argcSim, argvSim, NULL); 	//log user input
 	}	
 }
@@ -29,24 +29,24 @@ int initialize(){
 //get a line of input from user and store in argcSim and argvSim
 //input should be formatted as cmd arg1 arg2 arg3 .... argn
 int getInput(int argcSim, char *argvSim[]){     //get a line of input from user
-	log("\ngetInput()");
+	logDebug("\ngetInput()");
 	char line[128], strOut[128];                //string to hold user input line
 	printf("enter command:");                   //display message to user
     fgets(line, 128, stdin);                    //get input line from user
-    log("got input:");
-    log(line);
+    logDebug("got input:");
+    logDebug(line);
 	line[strlen(line)-1] = 0;                   //kill \n at the end of line
 	argcSim = 1;								//count how many args are in input. need n+1
 	for(int i = 0; i < strlen(line); i++){		//loop throught input char by char
-		if(strcmp(line[i]," ")){ 
+		if(line[i] == (int)' '){ 
 		sprintf(strOut, "found arg %d at index %d", argcSim, i); 
-		log(strOut);
+		logDebug(strOut);
 		argcSim++;		//count how many " " are in user input line
 		}
 	}
 	char *newArray[argcSim];					//create a new array of string pointers of size argc
 	argvSim = newArray;							//make argv point to new array
-	log("fresh array:")
+	logDebug("fresh array:");
 	logArgEnv(argcSim, argvSim, NULL);
 	char temp[128];								//temporary string to to hold line
 	strcpy(temp, line);
@@ -57,7 +57,7 @@ int getInput(int argcSim, char *argvSim[]){     //get a line of input from user
 		argvSim[i] = token;
 		i++;
 		token = strtok(0, " ");
-		log("add arg")
+		logDebug("add arg");
 		logArgEnv(argcSim, argvSim, NULL);
 	}
 }
@@ -73,7 +73,7 @@ int logReset(){
 }
 
 //add str to debug.log file for easy debugging
-int log(char *str){
+int logDebug(char *str){
 	FILE *fp = fopen("debug.log", "a");		// fopen a FILE stream for APPEND
     fprintf(fp, "%s\n",str);				// append str to file
     fclose(fp);								// close FILE stream when done
@@ -83,17 +83,17 @@ int log(char *str){
 int logArgEnv(int argc, char *argv[], char *env[ ]){
 	char strOut[128];
 	sprintf(strOut, "argc = %d", argc);
-	log(strOut);
+	logDebug(strOut);
 	int i = 0;
 	while(argv[i]){ 
 		sprintf(strOut, "argv[%d] = %s", i, argv[i]); 
-		log(strOut);
+		logDebug(strOut);
 		i++; 
 	}
 	i = 0; 
 	while(env[i]){ 
 		sprintf(strOut, "env[%d] = %s", i, env[i]); 
-		log (strOut);
+		logDebug (strOut);
 		i++; 
 	}	 
 }
--- shsim/shsim.h
+++ shsim/shsim.h
@@ -10,5 +10,5 @@ int getInput();			//get a line of input from user
 
 /********functions for writing to debug.log file**********/
 int logReset();			//resets debug.log file
-int log(char *str);		//write a line to log file for debuging
+int logDebug(char *str);		//write a line to log file for debuging
 int logArgEnv(int argc, char *argv[], char *env[ ]);	//prints the arguments passed into main
