940b02ae747e62da9eafd1c9203c134e5eb755d8
lewis <lewis@LAPTOP-O3044F35>
2021-02-19 08:55:55 -0700

    debug

--- shsim/shsim.c
+++ shsim/shsim.c
@@ -143,7 +143,7 @@ void logArgEnv(int argc, char *argv[], char *env[ ]){
 	}
 	i = 0; 
 	while(env && env[i]){
-		sprintf(strOut, "p:env=%p,	p:env[%d] = %p",env, i, env[i]);
+		sprintf(strOut, "p:env=%p,	p:env[%d] = %p,	env[%d]= %s",env, i, env[i], i, env[i]);
 		logDebug (strOut, tabs);
 		i++;
 	} 
