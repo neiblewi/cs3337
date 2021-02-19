78d553757826aad65a1bdd4812bbfc905b665e64
lewis <lewis@LAPTOP-O3044F35>
2021-02-18 21:43:32 -0700

    debug

--- shsim/shsim.c
+++ shsim/shsim.c
@@ -18,9 +18,9 @@ int main( int argc, char *argv[], char *env[ ]){
 		char **argVector = NULL;				//empty pointer to array of strings to simulate argv
 		char line[128];							//string to hold user input line
 		getInput(line, &argCount, &argVector);	//get input from user
-		if(strcmp(argVector[0],"exit")){		//if first argument is "exit"
+		if(!strcmp(argVector[0],"exit")){		//if first argument is "exit"
 			exit(0);							//exit program
-		}else if (strcmp(argVector[0],"cd")){	//if first argument is "cd"
+		}else if (!strcmp(argVector[0],"cd")){	//if first argument is "cd"
 			int error;
 			if (argVector[1])					//if there is a seccond argument
 				error = chdir(argVector[1]);	//try to change to path in 2nd arg
