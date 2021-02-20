4f5b4515b0c8a03875b77f9ce5ccd17907ea6835
lewis <lewis@LAPTOP-O3044F35>
2021-02-20 09:03:38 -0700

    debug

--- ch3/c3.1.c
+++ ch3/c3.1.c
@@ -1,5 +1,11 @@
 /********************* C3.1. c: fork() ************************/ 
+
 #include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>  
+
+char *dir[64], *myargv[ 64]; // assume at most 64 parameters 
+char cmd[128]; 
 
 int main() { 
 	int pid; 
@@ -10,5 +16,22 @@ int main() {
 	} 
 	else{ // child executes this part (3). 
 		printf(" this is process %d parent =%d\n", getpid(), getppid()); 
+		int i, r; 
+		printf("THIS IS PROCESS %d IN %s\n", getpid(), argv[ 0]); 
+		if (argc < 2){ 
+			printf("Usage: a.out command [options]\n"); 
+			exit(0); 
+		} 
+		printf("argc = %d\n", argc); 
+		for (i = 0; i < argc; i++) // print argv[ ] strings 
+			printf("argv[% d] = %s\n", i, argv[ i]); 
+		for (i = 0; i < argc - 1; i++) // create myargv[ ] 
+			myargv[ i] = argv[ i + 1]; 
+		myargv[i] = 0; // NULL terminated array 
+		strcpy(cmd, "/bin/"); // create /bin/ command 
+		strcat(cmd, myargv[ 0]); 
+		printf("cmd = %s\n", cmd); // show filename to be executed 
+		r = execve( cmd, myargv, env); // come to here only if execve() failed 
+		printf("execve() failed: r = %d\n", r); 
 	} 
 }
