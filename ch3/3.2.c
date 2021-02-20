834e9ada15d4ab7b3583efd3086fbdb896920878
lewis <lewis@LAPTOP-O3044F35>
2021-02-19 22:43:01 -0700

    debug

--- ch3/3.2.c
+++ ch3/3.2.c
@@ -1,16 +1,16 @@
 /***************** C3.2. c file ********************/ 
-#include < stdio.h > 
+#include <stdio.h> 
 
 int main() { 
 	int pid = fork(); // fork a child 
 	if (pid){ // PARENT 
-		printf(“ PARENT %d CHILD =% d\ n”, getpid(), pid); 
+		printf("PARENT %d CHILD =%d\n", getpid(), pid); 
 		// sleep( 1); // sleep 1 second = = > let child run next 
-		printf(" PARENT %d EXIT\ n", getpid()); 
+		printf(" PARENT %d EXIT\n", getpid()); 
 	} 
 	else{ // child 
-		printf(" child %d start my parent =% d\ n", getpid(), getppid()); 
+		printf(" child %d start my parent =%d\n", getpid(), getppid()); 
 		// sleep( 2); // sleep 2 seconds = > let parent die first 
-		printf(" child %d exit my parent =% d\ n", getpid(), getppid()); 
+		printf(" child %d exit my parent =%d\n", getpid(), getppid()); 
 	} 
 }
