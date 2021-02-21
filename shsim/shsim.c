cc53da2f44f1030a151db61cfd395bd4c17a895e
lewis <lewis@LAPTOP-O3044F35>
2021-02-21 10:54:00 -0700

    cleanup

--- shsim/shsim.c
+++ shsim/shsim.c
@@ -92,7 +92,7 @@ void executeCommand(int argCount, char **argVector, char **env){
 	logDebug(logStrOut, logTabs);
 	
 	int envPathCount, i;							//length of array
-	strArrCount(env, envPathCount, ':');			//get length of array
+	strArrCount(envPath, envPathCount, ':');			//get length of array
 /*
 	envPathCount = 1;								//count how many directories are in path. need at least one
 	for (i = 0; i < strlen(envPath); i++) {			//loop throught input char by char
