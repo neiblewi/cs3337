512693374dbbaef3516fe3998b707ea40bd3a60d
lewis <lewis@LAPTOP-O3044F35>
2021-02-22 20:44:44 -0700

    debug

--- shsim/test.c
+++ shsim/test.c
@@ -35,13 +35,18 @@ int main(int argc, char* argv[], char* env[]) {
 	printf("didnt work\n");
 */
 
-
-	close(1); open("outfile", O_WRONLY|O_CREAT, 0644);
+/*	close(1); open("outfile", O_WRONLY|O_CREAT, 0644);
 	char* cmd = "/bin/echo";
 	char* argVector[] = { "echo", "hello", "world", NULL };
 	int r = execve(cmd, argVector, env);
 	printf("didnt work\n");
+*/
 
+	close(1); open("outfile", O_APPEND | O_CREAT, 0644);
+	char* cmd = "/bin/echo";
+	char* argVector[] = { "echo", "hello", "world", NULL };
+	int r = execve(cmd, argVector, env);
+	printf("didnt work\n");
 
 }
 
