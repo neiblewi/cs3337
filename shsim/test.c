76cce2c94e0acea51e867bb6dfd98b25d1282e57
lewis <lewis@LAPTOP-O3044F35>
2021-02-22 22:18:41 -0700

    debug

--- shsim/test.c
+++ shsim/test.c
@@ -10,30 +10,23 @@ void strTrim(char* str) {
 	int readIndex = 0;
 	int writeIndex = 0;
 	while (str[readIndex] == ' ') {			//loop past all leading spaces
-		printf("skip[%i]\n", readIndex);
 		readIndex++;
 	}
 	while (str[readIndex] != '\0') {			//loop till end of string
 		printf("	|%s|\n", str);
 		if (str[readIndex] == '<' || str[readIndex] == '>') { //if < or >
-			printf("<>\n");
 			while (str[readIndex] == ' ') {
 				readIndex++;				//skip spacesafter > or <
 			}
 		}
 		if (str[readIndex] == ' ' && str[readIndex + 1] == ' ') {	//if there are two spaces in a row
-			printf("|  |\n");
 			readIndex++;					//skip extra whitespace
 		}
 		else{									//if other character or single space
-			printf("other\n");
-			printf("write[%i]%c\n", writeIndex, str[writeIndex]);
-			printf("write[%i]%c = read[%i]%c\n", writeIndex, str[writeIndex], readIndex, str[readIndex]);
 			str[writeIndex] = str[readIndex];	//copy read to write
 			writeIndex++;
 		}
 		readIndex++;
-		printf("end\n");
 	}
 	str[writeIndex] = '\0';					//end string with null character
 }
