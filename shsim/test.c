b8288e86fd3db00ed86b4a1e00b3c936efd5d49e
lewis <lewis@LAPTOP-O3044F35>
2021-02-22 17:54:43 -0700

    debug

---
+++ shsim/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>	
+#include <stdarg.h>
+#include <unistd.h>
+int main(int argc, char* argv[], char* env[]) {
+	char* cmd = "echo";
+	char** argVector = { "echo", "hello", "world", NULL };
+	int r = execve(cmd, argVector, env);
+}
+
