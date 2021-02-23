c19c3d3cdce59449eeeb4edf8bba5a8de866678c
lewis <lewis@LAPTOP-O3044F35>
2021-02-22 18:09:08 -0700

    debug

--- shsim/test.c
+++ shsim/test.c
@@ -5,9 +5,19 @@
 #include <stdarg.h>
 #include <unistd.h>
 int main(int argc, char* argv[], char* env[]) {
-	char* cmd = "/bin/echo";
+/*	char* cmd = "/bin/echo";
 	char* argVector [] = { "echo", "hello", "world", NULL };
 	int r = execve(cmd, argVector, env);
 	printf("didnt work\n");
+*/
+	char* cmd = "/usr/bin/gcc";
+	char* argVector[] = { "gcc", "t.c", NULL };
+	int r = execve(cmd, argVector, env);
+	printf("didnt work\n");
+
+
+
+
+
 }
 
