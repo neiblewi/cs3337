4db8a2d0e90718591186ad8f5e2f38102d47c863
lewis <lewis@LAPTOP-O3044F35>
2021-02-22 18:24:43 -0700

    debug

--- shsim/test.c
+++ shsim/test.c
@@ -26,12 +26,23 @@ int main(int argc, char* argv[], char* env[]) {
 	printf("didnt work\n");
 */
 
-	FILE* fp = fopen("outfile", "w");		// fopen a FILE stream for 
+/*	FILE* fp = fopen("outfile", "w");		// fopen a FILE stream for 
 	fclose(fp);
 	close(1); open("outfile", O_WRONLY, 0644);
 	char* cmd = "/bin/echo";
 	char* argVector[] = { "echo", "hello", "world", NULL };
 	int r = execve(cmd, argVector, env);
 	printf("didnt work\n");
+*/
+
+	FILE* fp = fopen("outfile", "a");		// fopen a FILE stream for 
+	fclose(fp);
+	close(1); open("outfile", O_WRONLY, 0644);
+	char* cmd = "/bin/echo";
+	char* argVector[] = { "echo", "hello", "world", NULL };
+	int r = execve(cmd, argVector, env);
+	printf("didnt work\n");
+
+
 }
 
