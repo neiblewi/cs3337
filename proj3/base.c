95ba8e52e6b6427212f11b68f3320ce1935b81cc
lewis <lewis@LAPTOP-O3044F35>
2021-02-27 22:31:51 -0700

    debug

--- proj3/base.c
+++ proj3/base.c
@@ -9,13 +9,15 @@
 #include <sys/time.h>
 
 #define LEN 128
-int pid; // child pid 
-int downPipe[2]; // pipe descriptors 
-int upPipe[2];	// pipe descriptors
-char line[LEN]; 
-time_t msgSend, msgReceive, replySend, replyReceive;
-int rplyRecieved;
+int pid;			// child pid 
+int downPipe[2];	// downpipe descriptors 
+int upPipe[2];		// uppipe descriptors
+char line[LEN];		// line to hold msg
+time_t msgSend, msgReceive, replySend, replyReceive;	//timestamps
+int rplyRecieved;	// 0 = no reply recieved. 1 = reply recieved
+struct itimerval timer;		//timer for timeout
 
+//change all aphabetic charcacters to uppercase.
 void toUpper(char* line) {
 	for (int i = 0; i < strlen(line); i++) {
 		if (line[i] >= (char)97 && line[i] <= (char)122)
@@ -23,12 +25,19 @@ void toUpper(char* line) {
 	}
 }
 
+//handler for sig 26
 void timer_handler(int sig) {
 	printf("parent % d got an interrupt sig = % d\n", getpid(), sig);
+	printf("parent %d send signal 10 to %d\n", getpid(), pid);
+	kill(pid, SIGUSR1);					// send signal to child process
 }
 
+//handler for sig  12
 void phandler(int sig) {
 	printf("parent %d got an interrupt sig =%d\n", getpid(), sig);
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 0;			
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);	// cancel VIRTUAL itimer 
 	//recieve reply
 	rplyRecieved = 1;
 	read(upPipe[0], line, LEN); // read pipe 
@@ -39,6 +48,7 @@ void phandler(int sig) {
 	printf("parent %d got message = %s\n", getpid(), line);
 }
 
+//handler for sig 11
 void chandler(int sig) { 
 	printf("child %d got an interrupt sig =%d\n", getpid(), sig); 
 	//get message
@@ -64,20 +74,20 @@ void chandler(int sig) {
 	kill(getppid(), SIGUSR2);		// send signal parent process 
 } 
 
+// main function for parent process
 int parent() { 
 	printf("parent %d running\n", getpid()); 
-	close(downPipe[0]); // parent = downpipe writer 
-	close(upPipe[1]); // parent = uppipe reader
-	signal(SIGUSR2, phandler); // install signal catcher for signal from child
-	signal(SIGVTALRM, timer_handler);
-	struct itimerval timer;
+	close(downPipe[0]);			// parent = downpipe writer 
+	close(upPipe[1]);			// parent = uppipe reader
+	signal(SIGUSR2, phandler);	// install signal catcher for signal from child
+	signal(SIGVTALRM, timer_handler);	// install signal catcher for signal from timer
 	while(1){ 
 		printf("parent %d: input a line : \n", getpid());
 		//get message
 		fgets(line, LEN, stdin);			// line from user
-		line[strlen(line) - 1] = 0;			// kill \n at end 
 		rplyRecieved = 0;
 		//prepare message
+		line[strlen(line) - 1] = 0;			// kill \n at end 
 		msgSend = time(NULL);				//get first time stamp
 		char temp[LEN];
 		sprintf(temp, "\n\tsendTS=%ld", msgSend);		//add timestamp
@@ -85,10 +95,11 @@ int parent() {
 		//send message
 		printf("parent %d write to pipe\n", getpid()); 
 		write(downPipe[1], line, LEN);		// write to pipe 
+		//signal child
+		printf("parent %d send signal 10 to %d\n", getpid(), pid); 
+		kill(pid, SIGUSR1);					// send signal to child process
+		//check for reply. resignal on timeout
 		while (!rplyRecieved) {
-			printf("parent %d send signal 10 to %d\n", getpid(), pid); 
-			kill(pid, SIGUSR1);					// send signal to child process
-	
 			//set timers
 			timer.it_value.tv_sec = 0;
 			timer.it_value.tv_usec = 100000;			// 10000 nsec = 10 msec
@@ -98,21 +109,21 @@ int parent() {
 	}
 } 
 
+// main function for child process
 int child() { 
 	printf("child %d running\n", getpid()); 
-	close(upPipe[0]); // child is pipe writer
-	close(downPipe[1]); // child is pipe reader 
-	signal(SIGUSR1, chandler); // install signal catcher 
-	while(1); 
-	//int parent = getppid(); 
-	//char msg[LEN]; 
+	close(upPipe[0]);			// child is pipe writer
+	close(downPipe[1]);			// child is pipe reader 
+	signal(SIGUSR1, chandler);	// install signal catcher 
+	while(1);					// continue running
 } 
 
+//main function for program
 int main() {
-	pipe(downPipe); // create a pipe 
-	pipe(upPipe);	//create pipe
-	pid = fork(); // fork a child process 
-	if (pid) // parent 
+	pipe(downPipe);		// create a pipe 
+	pipe(upPipe);		//create pipe
+	pid = fork();		// fork a child process 
+	if (pid) 
 		parent();
 	else
 		child();
